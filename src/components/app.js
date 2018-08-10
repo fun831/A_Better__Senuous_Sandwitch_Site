d201fbb6c423d9f43498a06a3b9cb5bfaa3eeb44
Matt Farnsworth <mryanfarnsworth@gmail.com>
2018-08-10 11:06:26 -0600

    fixed typo

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Sensuous Sandwitch!</h1>
+        <h1>Sensuous Sandwich!</h1>
       </div>
     );
   }
