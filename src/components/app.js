b5770b212917362d95337fb905e08e0024f87b01
Selma B <selmsb@gmail.com>
2018-08-10 14:54:22 -0600

    created slideShow component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Homepage from './homepage';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Sensuous Sandwich!</h1>
+        <Homepage/>
       </div>
     );
   }
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import SlideShow from './slideShow';
 
 export default class Homepage extends Component {
 	 render() {
 		return (
 			<div className=''>
-				
+				<SlideShow />
+				<div>Menu Link</div>
+				<div>About Us</div>
 			</div>
 		);
 	}
