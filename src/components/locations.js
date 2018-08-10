31554519324f2fdc9172017edea448b927e2bdfe
Matthew Warner <mattwarner14@yahoo.com>
2018-08-10 15:22:25 -0600

    organized hard code and pushing for fun

--- src/components/locations.js
+++ src/components/locations.js
@@ -1,35 +1,27 @@
 import React, { Component } from 'react';
 import Navbar from './navbar';
 
-import Location from './location';
-// import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export default class Locations extends Component {
 	 render() {
 		return (
-			<div className=''>
+			<div className = "Text">
 				<Navbar/>
-				<h2>Our Locations</h2>
-				{Location("Provo", "Downtown 163 West Center Street Provo, UT 84601-4431", "(801) 377-9244", "Mon-Sat: 10am-10pm; Closed Sun")}
-				{Location("Orem", "1680 S State St Orem, UT 84097-8070 801-225-9475", "(801) 225-9475", "Mon-Sat: 10am-10pm; Closed Sun")}
-				<div>
-					{/* <Map google={this.props.google} zoom={14}>
-	
-						<Marker onClick={this.onMarkerClick}
-									name={'Current location'} />
-	
-						<InfoWindow onClose={this.onInfoWindowClose}>
-							<div>
-								<h1>{this.state.selectedPlace.name}</h1>
-							</div>
-						</InfoWindow>
-					</Map> */}
-				</div>
+
+				<h2 className = "Title">Our Locations</h2>
+					<div className = "Provo"> 
+						<h4>Provo Location</h4>
+						<p>1680 S State St Orem, UT 84097-8070</p>
+						<p>(801) 377-9244</p>
+						<p>Mon-Sat: 10:30am-8pm; Closed Sunday</p>
+					</div>
+					<div className = "Orem">
+						<h4>Orem Location</h4>
+						<p>Downtown 163 West Center Street Provo, UT 84601-4431</p>
+						<p>(801) 225-9475</p>
+						<p>Mon-Sat: 10:30am-8pm; Closed Sunday</p>
+					</div>
 			</div>
 		);
 	}
 }
-
-// export default GoogleApiWrapper({
-//   apiKey: (YOUR_GOOGLE_API_KEY_GOES_HERE)
-// })(MapContainer)
