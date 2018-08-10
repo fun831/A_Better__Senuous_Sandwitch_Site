9453b066cb51237784b02d8dc2002a024b909db1
Peter M Souza Jr <globar10@Peters-MacBook-Pro-2.local>
2018-08-10 11:06:38 -0700

    got general layout of location page

--- src/components/locations.js
+++ src/components/locations.js
@@ -6,6 +6,19 @@ export default class Locations extends Component {
 		return (
 			<div className=''>
 				<Navbar/>
+				<h4>list of locations</h4>
+				<div className="location">
+					Downtown 163 West Center Street Provo, UT 84601-4431
+				</div>
+				<div className="location">
+					1680 S State St Orem, UT 84097-8070 801-225-9475
+				</div>
+				<div>
+					MAP
+				</div>
+				<div>
+					Phone number and hours
+				</div>
 			</div>
 		);
 	}
